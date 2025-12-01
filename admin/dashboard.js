f8f4299e7687b815223ea86ba5a1f5691f7c3a3e
Emperorcray <155745445+Emperorcray@users.noreply.github.com>
2025-12-01 15:53:13 +0100

    commit

---
+++ admin/dashboard.js
@@ -0,0 +1,6 @@
+function dashboard(){
+  document.getElementById("home").style.display= "block";
+  document.getElementById("products").style.display= "none";
+  document.getElementById("orders").style.display= "none";
+  
+}
